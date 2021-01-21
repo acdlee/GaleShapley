efa121d6e576547aa0597fa7a1bb46dc89ffab7b
Christopher Lee <clee651@terpmail.umd.edu>
2021-01-21 11:08:23 -0500

    Solved a bug dealing with input formatting; new bug with failed test.

--- test_gale_shapley.py
+++ test_gale_shapley.py
@@ -39,11 +39,48 @@ class TestGS(unittest.TestCase):
 				[7,2,3,4,1,6,5], [4,6,2,3,1,7,5],
 				[6,7,5,2,4,3,1], [5,4,1,7,3,2,6], [7,6,3,4,5,1,2]]
 
+		#since this input is structured as a priority queue
+		#(ie, prefA[0][0] is the candidate number for the highest
+		#rated candidate), we have to restructure the input
+		new_prefA, new_prefB = self.restructure_input(prefA, prefB)
+
 		#expected result
 		expected_result = ([2, 3, 1, 7, 6, 4, 5], [3, 1, 2, 6, 7, 5, 4])
 
 		result = gs(prefA, prefB)
 		self.assertEqual(result, expected_result)
+
+	def restructure_input(self, prefA, prefB):
+		"""
+		Takes the current input, where prefA[0][0] is the 
+		candidate number of the highest favored candidate
+		for employer 0, and restructures it such that prefA[0][0]
+		is the ranking of candidate 0 for employer 0. 
+
+		O(n) time to restrucutre each individual list. 
+		2n total lists that need restructuring. 
+		Time complexity of restructuring the input:
+			O(n^2)
+		Time complexity of restructuring the input and
+		running the algorithm:
+			T(n) = O(n^2 + n^2),
+			T(n) = O(n^2)
+		"""
+		new_prefA = [self.restructure_helper(lst) for lst in prefA]
+		new_prefB = [self.restructure_helper(lst) for lst in prefB]
+
+		return new_prefA, new_prefB
+
+	def restructure_helper(self, lst):
+		"""
+		Helper function for restructure_input().
+		Most of the grunt work is done here.
+		"""
+		n = len(lst)
+		tmp_lst = [0] * n
+		for i in range(0, n):
+			tmp_lst[lst[i] - 1] = i + 1
+		return tmp_lst
 
 if __name__ == '__main__':
 	unittest.main()
